259bd867ff441737a5be2c3925b291809f042a73
anonymax <32836662+natin56y@users.noreply.github.com>
2020-04-23 11:09:14 +0200

    add - show mongoDB connection

--- index.js
+++ index.js
@@ -8,21 +8,27 @@ const app = express();
 const routes = require('./routes');
 
 async function startServer() {
+    try {
+        console.log(`Trying to connect to mongoDB : ${process.env.MONGO_URI} ...`);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useUnifiedTopology: true,
+            authSource: 'admin'
+        });
 
-    await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        authSource: 'admin'
-    });
+        console.log(`Connection successful to mongoDB`);
 
-    app.use(cors());
+        app.use(cors());
 
-    routes(app);
+        routes(app);
 
-    app.listen(process.env.PORT, () => {
-        console.log(`Started server on port : ${process.env.PORT} ...`);
-    });
+        app.listen(process.env.PORT, () => {
+            console.log(`Started server on port : ${process.env.PORT} ...`);
+        });
+    }catch (e) {
+        console.log(`Server error : ${e.message}`);
+    }
 }
 
 startServer().then();
