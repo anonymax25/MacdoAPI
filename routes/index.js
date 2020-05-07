11694499556f8aa546e48cec801f2e250f187b55
anonymax <32836662+natin56y@users.noreply.github.com>
2020-05-07 13:26:45 +0200

    fix - remove unsed route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = function (app) {
     require('./auth.route')(app);
-    require('./project.route')(app);
     require('./user.route')(app);
     require('./command.route')(app);
     require('./admin')(app);
