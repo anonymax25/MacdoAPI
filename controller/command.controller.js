55712e4fb820b1ae1ea5628e77cbd8e06028cb43
Juliien <julien1105@outlook.fr>
2020-05-08 17:07:59 +0200

    update valid and update staff

--- controller/command.controller.js
+++ controller/command.controller.js
@@ -5,12 +5,12 @@ const Product = models.Product;
 
 class CommandController {
 
-    static async add(customer,products,menus,staff,price) {
+    static async add(customer,products,menus,price) {
         const command = new Command({
             customer,
             products,
             menus,
-            staff,
+            staff: null,
             isValid: false,
             price
         });
@@ -27,6 +27,24 @@ class CommandController {
     static async getById(id) {
         const command = await Command.findOne({_id: id}).populate('user').populate('products').populate('menus');
         return command;
+    }
+
+    static async isValidated(id) {
+        const command = await Command.findOne({_id: id});
+        const res = await Command.updateOne({_id: id}, $set: {isValid: true});
+        if(nModified == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    static async isAssigned(id, staff_id) {
+         const command = await Command.findOne({_id: id});
+         const res = await Command.updateOne({_id: id},  $set: {staff: staff_id});
+         if(nModified == 0) {
+             return true;
+         }
+         return false;
     }
 }
 
--- routes/command.route.js
+++ routes/command.route.js
@@ -1,28 +1,27 @@
 const bodyParser = require('body-parser');
 
 const CommandController = require('../controller').CommandController;
+const AuthMiddleware = require('../middleware').AuthMiddleware;
 
 module.exports = function (app) {
     app.post('/command', bodyParser.json(), async (req, res) => {
         try {
-            const command = await CommandController.add(req.body.customer, req.body.products, req.body.menus, req.body.staff, req.body.price);
+            const command = await CommandController.add(req.body.customer, req.body.products, req.body.menus, req.body.price);
             if (command) {
                 res.status(201).json(command);
             } else {
-                res.status(409).end();
+                res.status(400).end();
             }
         } catch (e) {
             res.status(500).json(e);
         }
     });
 
-    app.get('/command', bodyParser.json(), async (req, res) => {
+    app.get('/command', AuthMiddleware.adminAuth(), bodyParser.json(), async (req, res) => {
         try {
             const products = await CommandController.getAll();
             if (products) {
                 res.status(200).json(products);
-            } else {
-                res.status(409).end();
             }
         } catch (e) {
             res.status(500).json(e);
@@ -31,14 +30,41 @@ module.exports = function (app) {
 
     app.get('/command/:id', bodyParser.json(), async (req, res) => {
         try {
-            const ingredient = await CommandController.getById(req.params.id);
+            const command = await CommandController.getById(req.params.id);
             if (ingredient) {
-                res.status(200).json(ingredient);
+                res.status(200).json(command);
             } else {
                 res.status(409).end();
             }
         } catch (e) {
             res.status(500).json(e);
         }
+    });
+
+    app.put('/command/valid/:id', bodyParser.json(), async (req, res) => {
+           try {
+               const command = await CommandController.isValidated(req.params.id);
+               if (command == true) {
+                   res.status(200).json(ingredient);
+               } else {
+                   res.status(409).end();
+               }
+           } catch (e) {
+               res.status(500).json(e);
+           }
+    });
+
+    app.put('/command/staff/:id', bodyParser.json(), async (req, res) => {
+           try {
+               const command = await CommandController.isAssigned(req.params.id, req.body.staff);
+               console.log(command);
+               if (command == true ) {
+                   res.status(200).json(ingredient);
+               } else {
+                   res.status(409).end();
+               }
+           } catch (e) {
+               res.status(500).json(e);
+           }
     });
 }
